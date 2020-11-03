ea7829c6b9b2715b389286b198daa837737cf427
AlanYHung <AlanYHung@hotmail.com>
2020-11-02 16:39:44 -0800

    Created a Store Hours function in app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-function addStore (storeArray, fpStoreLocation, fpMinCust, fpMaxCust, fpAvgSales){
+'use strict';
+
+var storeArray = [];
+
+function addStore (addStoreArray, addStoreLocation, addMinCust, addMaxCust, addAvgSales){
     var storeObject = {
-        storeLocation: fpStoreLocation,
-        minCust: fpMinCust,
-        maxCust: fpMaxCust,
-        avgSales: fpAvgSales,
+        storeLocation: addStoreLocation,
+        minCust: addMinCust,
+        maxCust: addMaxCust,
+        avgSales: addAvgSales,
         randCustPerHour: function(min, max){
             //Code Taken from
             //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -13,14 +17,53 @@ function addStore (storeArray, fpStoreLocation, fpMinCust, fpMaxCust, fpAvgSales
         }
     }
 
-    storeArray.push(storeObject)
-    return storeArray;
+    addStoreArray.push(storeObject)
+    return addStoreArray;
 }
 
-var storeArray = [];
+//This function requires Opening and Closing to be put in 24 hour system (Military Time)
+function cookiesSoldPerHour(csphStoreArray, csphOpening, csphClosing){
+    var i = 0;
+    var storeHours = [];
+    var cookiesSold = 0;
+    var cookiesSoldTotal = 0;
+    var currHour = csphOpening;
+    var dispHour = currHour;
+    var strAMPM = 'AM';
+
+    // console.log(csphStoreArray);
+    // console.log(csphStoreArray[0].storeLocation);
+
+    while(currHour !== csphClosing){
+        if(currHour === 12){
+            dispHour = currHour;
+            strAMPM = 'PM';
+        }else if(currHour === 24){
+            dispHour = currHour - 12;
+            strAMPM = 'AM';
+            currHour = 0;
+        }else if(currHour > 12){
+            dispHour = currHour - 12;
+        }else{
+            dispHour = currHour;
+        }
+        
+        cookiesSold = Math.round(csphStoreArray[0].randCustPerHour(csphStoreArray[0].minCust,csphStoreArray[0].maxCust) * csphStoreArray[0].avgSales);
+        cookiesSoldTotal = cookiesSoldTotal + cookiesSold;
+        storeHours.push(dispHour + strAMPM + ': ' + cookiesSold + ' cookies');
+        currHour++;
+    }
+    // console.log(storeHours);
+
+    return storeHours;
+}
+
+// function 
 
 storeArray = addStore(storeArray,'Seattle',23,65,6.3);
 storeArray = addStore(storeArray,'Tokyo',3,24,1.2);
 storeArray = addStore(storeArray,'Dubai',11,38,3.7);
 storeArray = addStore(storeArray,'Paris',20,38,2.3);
 storeArray = addStore(storeArray,'Lima',2,16,4.6);
+
+// console.log(cookiesSoldPerHour(storeArray,6,20));
